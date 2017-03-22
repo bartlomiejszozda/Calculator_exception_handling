9470df5a4699a3bcb55b2bcee32d1dbeac2760e2
bartlomiejszozda <bartekszozda19@gmail.com>
2017-03-22 13:49:34 +0100

    Add files via upload

---
+++ calculator/Exceptions.py
@@ -0,0 +1,10 @@
+class Not_A_String(Exception):
+    pass
+class Not_A_Number(Exception):
+    pass
+class Not_A_Possible_Denominator(Exception):
+    pass
+class Not_A_Possible_Basis(Exception):
+    pass
+class Not_A_Positive_Value(Exception):
+    pass
